bf01d8bb79494efa1f31588132e6f2a81d8b9612
vagrant <vagrant@vagrant.vm>
2020-10-05 16:21:35 +0000

    functions tested

--- wordcount.py
+++ wordcount.py
@@ -9,15 +9,16 @@ def word_count(file):
     words_list = []
     for line in file:
         line = line.rstrip()
-        words = line.split('')
+        words = line.split(' ')
         for word in words:
             words_list.append(word)
     words_dic = make_dic(words_list)
     for key, value in words_dic.items():
-        print("key = {}, value = {}".format(key, value))
+        print("{} {}".format(key, value))
     
 def make_dic(words):
             checking_words = {}
             for word in words:
                 checking_words[word] = checking_words.get(word,0) + 1
             return checking_words
+
